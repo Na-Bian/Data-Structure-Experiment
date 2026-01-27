2254e756af5542c5c42fced3289535926289f311
Na Bian <3157668135@qq.com>
2026-01-27 17:55:53 +0800

    Create List.h

---
+++ 算法实现/链表与线性表/单链表/List.h
@@ -0,0 +1,27 @@
+# ifndef LIST_LIST_H
+# define LIST_LIST_H
+
+//定义数据类型
+typedef union Data {
+    int int_data;
+    char char_data;
+} Data;
+
+//定义链表节点类型
+typedef struct Node {
+    Data data;
+    struct Node *next;
+} Node, *List;
+
+
+//定义枚举类型DataType，标识链表数据类型
+typedef enum DataType {
+    INT,
+    CHAR
+} DataType;
+
+List init_list(DataType); //初始化链表
+void print_list(List, DataType); //遍历输出链表
+void build_list(List, int, DataType); //建立长度为n的链表
+
+# endif //LIST_LIST_H
