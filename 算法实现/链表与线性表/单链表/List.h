1b1c8d112cc51a47a10dcef0aedb85fa36d96655
Na Bian <3157668135@qq.com>
2026-01-27 18:04:42 +0800

    Update List.h

--- 算法实现/链表与线性表/单链表/List.h
+++ 算法实现/链表与线性表/单链表/List.h
@@ -28,4 +28,11 @@ List init_list(DataType); //初始化链表
 void print_list(List, DataType); //遍历输出链表
 void build_list(List, int, DataType); //建立长度为n的链表
 
+Node *find_last_k_node(List, int); //查找链表中倒数第k个节点
+Node *find_start_of_common_suffix(List, List); //查找具有相同后缀的两个单词，共同后缀的起始位置
+void delete_middle_node(List); //删除链表中间的节点（长度为奇数且大于2）
+List reverse_list(List, DataType); //反转链表（元素个数>2）
+Node *find_entry_of_loop(List); //判断链表是否有环并找到环的入口
+void remove_duplicate_nodes(List, int); //将链表中data绝对值相等的节点仅保留第一次出现的节点，已知|data| <= n
+
 # endif //LIST_LIST_H
